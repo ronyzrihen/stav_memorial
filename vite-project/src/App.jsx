f05e34ffaafc0e60a38ed60c8be49fc404d0eb65
ronyzrihen <ronyzn3@gmail.com>
2024-04-25 01:07:32 +0300

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import {useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <header>
+                <img src={reactLogo} alt="React Logo"/>
+                <h1>סתיו ברזני ז״ל</h1>
+            </header>
+            <h2>מנציחים את סתיו ברזני </h2>
+        </>
+)
+}
+
+export default App
